ef67261057c2803d52836050179a73fec0de9c32
AdaPirjol <pirjol.adelina08@gmail.com>
2024-08-08 11:40:06 +0300

    read write
    Signed-off-by: AdaPirjol <pirjol.adelina08@gmail.com>

--- day1/main.c
+++ day1/main.c
@@ -1,25 +1,36 @@
 #include <stdio.h>
 #include <iio.h>
-#include <string.h>
-#define URI "ip:10.76.84.239"
-int main()
-{
-    unsigned int major, minor;
-    char git_tag[8];
-    iio_library_get_version(&major, &minor, git_tag);
-    printf("IIO library version: %d.%d-%s\n", major, minor, git_tag);
+#include <unistd.h>
+#define URI "ip:10.76.84.108"
+#define DEVICE_NAME "iio:device0"
+#define CHANNEL_NAME "voltage0"
+#define ATTRIBUTE_NAME "raw"
+int main() {
     struct iio_context *ctx = iio_create_context_from_uri(URI);
     if (!ctx) {
-        perror("No context");
+        perror("Creating context from URI failed");
         return -1;
     }
-    // iio_context_get_attrs_count(ctx);
-    int n = iio_context_get_attrs_count(ctx);
-    printf("Number of attributes: %d\n", n);
-    for (int i = 0; i < n; i++) {
-        const char *value;
-        const char *name;
-        int attr = iio_context_get_attr(ctx, i, &name, &value);
-        printf("Attribute %s: %s\n", name, value);
+    struct iio_device *device = iio_context_find_device(ctx, DEVICE_NAME);
+    if (!device) {
+        fprintf(stderr, "Device %s not found\n", DEVICE_NAME);
+        iio_context_destroy(ctx);
+        return -1;
     }
+    struct iio_channel *channel = iio_device_find_channel(device, CHANNEL_NAME, false);
+    if (!channel) {
+        fprintf(stderr, "Channel %s not found\n", CHANNEL_NAME);
+        iio_context_destroy(ctx);
+        return -1;
+    }
+    char buf[16];
+    for (int value = 1; value <= 10; value++) {
+        snprintf(buf, sizeof(buf), "%d", value);
+        iio_channel_attr_write(channel, ATTRIBUTE_NAME, buf);
+        printf("Set %s to %d\n", ATTRIBUTE_NAME, value);
+        iio_channel_attr_read(channel, ATTRIBUTE_NAME, buf, sizeof(buf));
+        printf("Read %s: %s\n", ATTRIBUTE_NAME, buf);
+        sleep(1);
+    }
+    return 0;
 }
