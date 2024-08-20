fe689a4ba999e47f4735c7b73fc472c8601f4cb2
MariaAlexandraM <maria_moldovan_2003@yahoo.com>
2024-08-20 11:17:05 +0300

    day3: improved shock detection when a key is pressed, stop the program and destroy context
    Signed-off-by: MariaAlexandraM <maria_moldovan_2003@yahoo.com>

--- day3/shock_detection.c
+++ day3/shock_detection.c
@@ -1,10 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <iio.h>
 
+#include <termios.h>
+#include <fcntl.h>
+
 #define URI "ip:10.76.84.236"
-#define THRESHOLD 0.01
+#define THRESHOLD 10
+#define CHANS_NB 6
+
+int kbhit(void) // https://stackoverflow.com/questions/32390617/get-keyboard-interrupt-in-c
+{
+    struct termios oldt, newt;
+    int ch;
+    int oldf;
+
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+    ch = getchar();
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+    if (ch != EOF)
+    {
+        ungetc(ch, stdin);
+        return 1;
+    }
+
+    return 0;
+}
 
 int main()
 {
@@ -24,142 +56,104 @@ int main()
         return -1;
     }
 
-    char *chan_0_name = "voltage0";
-    char *chan_1_name = "voltage1";
-    char *chan_2_name = "voltage2";
-    char *chan_3_name = "voltage3";
-    char *chan_4_name = "voltage4";
-    char *chan_5_name = "voltage5";
+    iio_device_attr_write(dev, "en", "1");
 
-    struct iio_channel *chan_0 = iio_device_find_channel(dev, chan_0_name, false);
-    struct iio_channel *chan_1 = iio_device_find_channel(dev, chan_1_name, false);
-    struct iio_channel *chan_2 = iio_device_find_channel(dev, chan_2_name, false);
-    struct iio_channel *chan_3 = iio_device_find_channel(dev, chan_3_name, false);
-    struct iio_channel *chan_4 = iio_device_find_channel(dev, chan_4_name, false);
-    struct iio_channel *chan_5 = iio_device_find_channel(dev, chan_5_name, false);
+    char *chan_name[CHANS_NB] = {"voltage0", "voltage1", "voltage2", "voltage3", "voltage4", "voltage5"};
 
-    if (!chan_0 || !chan_1 || !chan_2 || !chan_3 || !chan_4 || !chan_5)
+    struct iio_channel *chans[CHANS_NB];
+
+    for (int i = 0; i < CHANS_NB; i++)
     {
-        perror("cannot get channel");
-        return -1;
-    }
-
-    const char *name = "raw";
-    char chan_0_data[20];
-    char chan_1_data[20];
-    char chan_2_data[20];
-    char chan_3_data[20];
-    char chan_4_data[20];
-    char chan_5_data[20];
-
-    iio_channel_attr_read(chan_0, name, chan_0_data, sizeof(chan_0_data));
-    iio_channel_attr_read(chan_1, name, chan_1_data, sizeof(chan_1_data));
-    iio_channel_attr_read(chan_2, name, chan_2_data, sizeof(chan_2_data));
-    iio_channel_attr_read(chan_3, name, chan_3_data, sizeof(chan_3_data));
-    iio_channel_attr_read(chan_4, name, chan_4_data, sizeof(chan_4_data));
-    iio_channel_attr_read(chan_5, name, chan_5_data, sizeof(chan_5_data));
-
-    int chan_0_num = atoi(chan_0_data);
-    int chan_1_num = atoi(chan_1_data);
-    int chan_2_num = atoi(chan_2_data);
-    int chan_3_num = atoi(chan_3_data);
-    int chan_4_num = atoi(chan_4_data);
-    int chan_5_num = atoi(chan_5_data);
-    int raw = chan_4_num;
-
-    while (true)
-    {
-        printf("init: %4d %4d %4d %4d %4d %4d\n", chan_0_num, chan_1_num, chan_2_num, chan_3_num, chan_4_num, chan_5_num);
-
-        struct iio_channel *chan_0_aux = iio_device_find_channel(dev, chan_0_name, false);
-        struct iio_channel *chan_1_aux = iio_device_find_channel(dev, chan_1_name, false);
-        struct iio_channel *chan_2_aux = iio_device_find_channel(dev, chan_2_name, false);
-        struct iio_channel *chan_3_aux = iio_device_find_channel(dev, chan_3_name, false);
-        struct iio_channel *chan_4_aux = iio_device_find_channel(dev, chan_4_name, false);
-        struct iio_channel *chan_5_aux = iio_device_find_channel(dev, chan_5_name, false);
-
-        if (!chan_0_aux || !chan_1_aux || !chan_2_aux || !chan_3_aux || !chan_4_aux || !chan_5_aux)
+        chans[i] = iio_device_find_channel(dev, chan_name[i], false);
+        if (!chans[i])
         {
             perror("cannot get channel");
             return -1;
         }
+    }
 
-        const char *name = "raw";
-        char chan_0_data_aux[20];
-        char chan_1_data_aux[20];
-        char chan_2_data_aux[20];
-        char chan_3_data_aux[20];
-        char chan_4_data_aux[20];
-        char chan_5_data_aux[20];
+    const char *attr_name = "raw";
+    char chan_data[CHANS_NB][20];
+    int chan_num[CHANS_NB];
 
-        iio_channel_attr_read(chan_0_aux, name, chan_0_data_aux, sizeof(chan_0_data_aux));
-        iio_channel_attr_read(chan_1_aux, name, chan_1_data_aux, sizeof(chan_1_data_aux));
-        iio_channel_attr_read(chan_2_aux, name, chan_2_data_aux, sizeof(chan_2_data_aux));
-        iio_channel_attr_read(chan_3_aux, name, chan_3_data_aux, sizeof(chan_3_data_aux));
-        iio_channel_attr_read(chan_4_aux, name, chan_4_data_aux, sizeof(chan_4_data_aux));
-        iio_channel_attr_read(chan_5_aux, name, chan_5_data_aux, sizeof(chan_5_data_aux));
+    for (int i = 0; i < CHANS_NB; i++)
+    {
+        iio_channel_attr_read(chans[i], attr_name, chan_data[i], sizeof(chan_data[i]));
+        chan_num[i] = atoi(chan_data[i]);
+    }
 
-        int chan_0_num_aux = atoi(chan_0_data_aux);
-        int chan_1_num_aux = atoi(chan_1_data_aux);
-        int chan_2_num_aux = atoi(chan_2_data_aux);
-        int chan_3_num_aux = atoi(chan_3_data_aux);
-        int chan_4_num_aux = atoi(chan_4_data_aux);
-        int chan_5_num_aux = atoi(chan_5_data_aux);
+    int raw = chan_num[4];
 
-        double chan_0_g = abs(chan_0_num_aux - chan_0_num) / (raw * 1.0);
-        double chan_1_g = abs(chan_1_num_aux - chan_1_num) / (raw * 1.0);
-        double chan_2_g = abs(chan_2_num_aux - chan_2_num) / (raw * 1.0);
-        double chan_3_g = abs(chan_3_num_aux - chan_3_num) / (raw * 1.0);
-        double chan_4_g = abs(chan_4_num_aux - chan_4_num) / (raw * 1.0);
-        double chan_5_g = abs(chan_5_num_aux - chan_5_num) / (raw * 1.0);
-
-        printf("loop: %4d %4d %4d %4d %4d %4d\n", chan_0_num_aux, chan_1_num_aux, chan_2_num_aux, chan_3_num_aux, chan_4_num_aux, chan_5_num_aux);
-        printf("G: %.4f %.4f %.4f %.4f %.4f %.4f\n", chan_0_g, chan_1_g, chan_2_g, chan_3_g, chan_4_g, chan_5_g);
-        printf("\tx: ");
-        if (chan_0_g > THRESHOLD)
+    while (!kbhit())
+    {
+        struct iio_channel *new_chans[CHANS_NB];
+        for (int i = 0; i < CHANS_NB; i++)
         {
-            printf("detected shock: %.4fG\n", chan_0_g);
-        }
-        else if (chan_1_g > THRESHOLD)
-        {
-            printf("detected shock: %.4fG\n", chan_1_g);
-        }
-        else
-        {
-            printf("no shock\n");
+            new_chans[i] = iio_device_find_channel(dev, chan_name[i], false);
+            if (!new_chans[i])
+            {
+                perror("cannot get channel");
+                return -1;
+            }
         }
 
-        printf("\ty: ");
-        if (chan_2_g > THRESHOLD)
+        char new_chan_data[CHANS_NB][20];
+        int new_chan_num[CHANS_NB];
+
+        for (int i = 0; i < CHANS_NB; i++)
         {
-            printf("detected shock: %.4fG\n", chan_2_g);
-        }
-        else if (chan_3_g > THRESHOLD)
-        {
-            printf("detected shock: %.4fG\n", chan_3_g);
-        }
-        else
-        {
-            printf("no shock\n");
+            iio_channel_attr_read(new_chans[i], attr_name, new_chan_data[i], sizeof(new_chan_data[i]));
+            new_chan_num[i] = atoi(new_chan_data[i]);
         }
 
-        printf("\tz: ");
-        if (chan_4_g > THRESHOLD)
+        double chan_g_values[CHANS_NB];
+
+        for (int i = 0; i < CHANS_NB; i++)
         {
-            printf("detected shock: %.4fG\n", chan_4_g);
+            chan_g_values[i] = abs(new_chan_num[i] - chan_num[i]) / (raw * 1.0);
         }
-        else if (chan_5_g > THRESHOLD)
+
+        printf("valori initiale:\n\t");
+        for (int i = 0; i < CHANS_NB; i++)
         {
-            printf("detected shock: %.4fG\n", chan_5_g);
+            printf("%7d ", chan_num[i]);
         }
-        else
+        printf("\n");
+
+        printf("valori noi:\n\t");
+        for (int i = 0; i < CHANS_NB; i++)
         {
-            printf("no shock\n");
+            printf("%7d ", new_chan_num[i]);
+        }
+        printf("\n");
+
+        printf("valori G:\n\t");
+        for (int i = 0; i < CHANS_NB; i++)
+        {
+            printf("%7.4f ", chan_g_values[i]);
+        }
+        printf("\n");
+
+        for (int i = 0; i < CHANS_NB; i++)
+        {
+            if (abs(new_chan_num[i] - chan_num[i]) >= THRESHOLD)
+            {
+                printf("\n\n");
+                if (i == 0 || i == 1)
+                    printf("x: ");
+                else if (i == 2 || i == 3)
+                    printf("y: ");
+                else
+                    printf("z: ");
+                printf("shock\n\n");
+            }
         }
 
         printf("------------------------------------\n");
         sleep(1);
     }
+
+    iio_context_destroy(ctx);
 
     return 0;
 }
