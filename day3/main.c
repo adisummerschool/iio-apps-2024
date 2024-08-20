f08b6b4ab0577a7b262b53fe564d3f3ffa3455ef
Volcov Sabina <volcovsabina@gmail.com>
2024-08-20 11:32:00 +0300

    Add SIGINT handler, change formula for shock detection
    Signed-off-by: Volcov Sabina <volcovsabina@gmail.com>

--- day3/main.c
+++ day3/main.c
@@ -1,17 +1,30 @@
 #include <iio.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <signal.h>
 #include <string.h>
-#define URI "ip:10.76.84.104"
+#define URI "ip:10.76.84.100"
 #define EN "en"
 #define CHAN_ATTR "raw"
 #define THRESHOLD 25
-#define SHOCK 500
+#define SHOCK 400
 #define G 2000
 #define DEVICE "ad5592r_s"
 
+struct iio_context *ctx;
+
+void sig_handler(int s) {
+	signal(SIGINT, sig_handler);
+	(void) s;
+	printf("\nDestroying context and exiting...\n");
+	iio_context_destroy(ctx);
+	exit(0);
+}
+
 
 int main() {
+	signal(SIGINT, sig_handler);
+
     	//mode selection
     	int mode = 0;
     	printf("Choose mode:\n  1 -> board calibration\n  2 -> shock detection\n");
@@ -22,7 +35,8 @@ int main() {
 	    return -1;
 	}
     	
-    	struct iio_context *ctx;
+    	//the context has to be a global variable in order to be destroyed from the SIGINT handler
+    	//struct iio_context *ctx;
         ctx = iio_create_context_from_uri(URI);
 
         if(!ctx) {
@@ -37,30 +51,36 @@ int main() {
     	//enabling the ADC
     	iio_device_attr_write(dev, EN, "1");
     	
-    	//running averages 
-    	long avg[6] = {0, 0, 0, 0, 0, 0};
-    	
-    	//continuously reading the channel values
-    	while(true) {
-    		char *chan_name[6] = {"voltage0",
+    	//initial values
+    	char *chan_name[6] = {"voltage0",
     					"voltage1", 
     					"voltage2",
     					"voltage3",
     					"voltage4",
     					"voltage5"};
+    	struct iio_channel* voltage[6];
+    	char init_raw[6][10];
+    	int init_raw_int[6];
+    	for (int i = 0; i < 6; i++) {
+	    	voltage[i] = iio_device_find_channel(dev, chan_name[i], NULL);
+	    	iio_channel_attr_read(voltage[i], CHAN_ATTR, init_raw[i], 10);  
+		init_raw_int[i] = atoi(init_raw[i]);
+ 		printf("%s channel initial raw value = %s\n", chan_name[i], init_raw[i]);
+	}
+	printf("\n\n");
     	
+    	//continuously reading the channel values
+    	while(true) {
 	    	char value[6][10] = {"0", "0", "0", "0", "0", "0"};
-	    	struct iio_channel* voltage[6];
 	    	int val[6];
 	    	
 	    	for (int i = 0; i < 6; i++) {
-	    		voltage[i] = iio_device_find_channel(dev, chan_name[i], NULL);
 	    		iio_channel_attr_read(voltage[i], CHAN_ATTR, value[i], 10);  
 	    		val[i] = atoi(value[i]);
 	    		printf("%s channel raw value = %s\n", chan_name[i], value[i]);
 	    	}
 	    	
-	    	printf("\n\n");
+	    	printf("\n");
 	    	
 	    	if (mode == 1) {
 		    	printf("Calibrating...\n\n");
@@ -97,80 +117,33 @@ int main() {
 	    	}
 	    	
 	    	if (mode == 2) {
-	    		for (int j = 0; j < 6; j++) {
-	    			avg[j] += val[j];
-	    			avg[j] /= 2;
+	    		char* axis_names[6] = {"X+",
+	    					"X-",
+	    					"Y+",
+	    					"Y-",
+	    					"Z+",
+	    					"Z-"};
+	    	
+	    		int dif;
+	    		for (int j = 0; j < 6; j++) {    	
+		    		if (val[j] - init_raw_int[j] >= SHOCK) {
+		    			dif = val[j] - init_raw_int[j];
+		    			printf("Shock on %s axis = %.2fg\n", axis_names[j], (float)dif/G);
+		    			printf("Recalibrate!\n");
+		    			sleep(5);
+		    			for (int i = 0; i < 6; i++) {
+	    					iio_channel_attr_read(voltage[i], CHAN_ATTR, init_raw[i], 10);  
+						init_raw_int[i] = atoi(init_raw[i]);
+					}
+		    		}
 	    		}
 		    	
-		    	int dif;
-		    	
-		    	if ((val[0] > avg[0] + SHOCK)) {
-		    		dif = val[0] - (avg[0] + SHOCK);
-		    		printf("Shock on X+ axis = %.2fg\n", (float)dif/G);
-		    	}
-		    	else if ((val[0] < avg[0] - SHOCK)) {
-		    		dif = (avg[0] - SHOCK) - val[0];
-		    		printf("Shock on X+ axis = %.2fg\n", (float)dif/G);
-		    	}
-		    	else if ((val[1] > avg[1] + SHOCK)) {
-		    		dif = val[1] - (avg[1] + SHOCK);
-		    		printf("Shock on X- axis = %.2fg\n", (float)dif/G);
-		    	}
-		    	else if ((val[1] < avg[1] - SHOCK)) {
-		    		dif = (avg[1] - SHOCK) - val[1];
-		    		printf("Shock on X- axis = %.2fg\n", (float)dif/G);
-		    	}
-		    	else {
-		    		printf("X: no shock\n");
-		    	}
-		    	
-		    	
-		    	if ((val[2] > avg[2] + SHOCK)) {
-		    		dif = val[2] - (avg[2] + SHOCK);
-		    		printf("Shock on Y+ axis = %.2fg\n", (float)dif/G);
-		    	}
-			else if ((val[2] < avg[2] - SHOCK)) {
-				dif = (avg[2] - SHOCK) - val[2];
-		    		printf("Shock on Y+ axis = %.2fg\n", (float)dif/G);
-		    	}
-		    	else if ((val[3] > avg[3] + SHOCK)) {
-		    		dif = val[3] - (avg[3] + SHOCK);
-		    		printf("Shock on Y- axis = %.2fg\n", (float)dif/G);
-		    	}
-		    	else if ((val[3] < avg[3] - SHOCK)) {
-		    		dif = (avg[3] - SHOCK) - val[3];
-		    		printf("Shock on Y- axis = %.2fg\n", (float)dif/G);
-		    	}
-		    	else {
-		    		printf("Y: no shock\n");
-		    	}
-		    	
-		
-		    	if ((val[4] > avg[4] + SHOCK) ) {
-		    		dif = val[4] - (avg[4] + SHOCK);
-		    		printf("Shock on Z+ axis = %.2fg\n", (float)dif/G);
-		    	}
-		    	else if ((val[4] < avg[4] - SHOCK)) {
-		    		dif = (avg[4] - SHOCK) - val[4];
-		    		printf("Shock on Z+ axis = %.2fg\n", (float)dif/G);
-		    	}
-		    	else if ((val[5] > avg[5] + SHOCK)) {
-		    		dif = val[5] - (avg[5] + SHOCK);
-		    		printf("Shock on Z- axis = %.2fg\n", (float)dif/G);
-		    	}
-		    	else if ((val[5] < avg[5] - SHOCK)) {
-		    		dif = (avg[5] - SHOCK) - val[5];
-		    		printf("Shock on Z- axis = %.2fg\n", (float)dif/G);
-		    	}
-		    	else {
-		    		printf("Z: no shock\n");
-		    	}
-	    	}
 	    	
-	    	printf("\n\n");	    	
-	    	sleep(1);
+	    		printf("\n");	    	
+	    		sleep(1);
+    		}
     	}
 
-        iio_context_destroy(ctx);
+        //iio_context_destroy(ctx);
 	return 0;
 }
