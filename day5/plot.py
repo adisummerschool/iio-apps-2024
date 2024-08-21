75642c1231a3f068b94e275d6cc1f473649613ed
Alexandru Andrei Alexa <alexaalexandru66@gmail.com>
2024-08-21 12:49:25 +0300

    Day5
    Signed-off-by: Alexandru Andrei Alexa <alexaalexandru66@gmail.com>

---
+++ day5/plot.py
@@ -0,0 +1,65 @@
+import iio
+import matplotlib.pyplot as plt 
+import array 
+import functii
+import numpy as np
+
+URI = "ip:10.76.84.243"
+
+ctx = iio.Context(URI)
+
+if not ctx:
+    print("No context")
+
+dev = ctx.find_device("iio:device0")
+
+if not dev:
+    print("no dev")
+
+#dev._write("en", "1")
+
+channels = []
+
+for i in range(6):
+    channels.append(dev.find_channel(f"voltage{i}"))
+
+for chn in channels:
+    chn.enabled = True
+
+buf = iio.Buffer(dev, 100, False)
+
+while True:
+
+    buf.refill()
+    bytes_buf = buf.read()
+
+    actual_data = []
+
+    for i in range(0, len(bytes_buf), 2):
+        sixteen_bit_number = functii.sixteen_bit(i, bytes_buf)
+        actual_data.append(sixteen_bit_number)
+
+    #print(np.array(actual_data).shape)
+    #arr = np.array(actual_data)
+    #print(arr.shape)
+
+    xpoz = np.array(actual_data[0::6])
+    xneg = np.array(actual_data[1::6])
+    ypoz = np.array(actual_data[2::6])
+    yneg = np.array(actual_data[3::6])
+    zpoz = np.array(actual_data[4::6])
+    zneg = np.array(actual_data[5::6])
+
+    X = xpoz - xneg
+    Y = ypoz - yneg
+    Z = zpoz - zneg
+
+    plt.plot(X, 'r')
+    plt.plot(Y, 'b')
+    plt.plot(Z, 'g')
+
+    plt.draw()
+    plt.pause(0.1)
+    plt.clf()
+
+#functii.functia_alex()
