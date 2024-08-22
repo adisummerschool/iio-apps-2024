b6be030e7a1301ae352e5ea966c83b63965666ef
Volcov Sabina <volcovsabina@gmail.com>
2024-08-22 14:54:39 +0300

    Backend code for board
    Signed-off-by: Volcov Sabina <volcovsabina@gmail.com>

--- day5/main.py
+++ day5/main.py
@@ -1,13 +1,15 @@
-from time import sleep
+from time import sleep, time
 import iio
 import math
-import numpy as np
 from pynput import keyboard
 from threading import Event, Thread
 from queue import Queue
 
 DEVICE_NAME = "ad5592r_s"
 URI = "ip:10.76.84.104"
+LOW = 5
+HIGH = 45
+PWM_TIMEOUT = 0.2
 
 #device acquisition from context
 def init_device():
@@ -53,27 +55,86 @@ def get_roll_pitch(axis_data):
     z_accel = float(axis_data['z']['+']) - float(axis_data['z']['-'])
 
     # Calculate roll and pitch
-    roll = math.atan2(y_accel, z_accel) * 180 / math.pi
-    pitch = math.atan2(-x_accel, math.sqrt(y_accel ** 2 + z_accel ** 2)) * 180 / math.pi
+    roll = 180 * math.atan2(y_accel, z_accel) / math.pi
+    pitch = 180 * math.atan2(-x_accel, math.sqrt(y_accel ** 2 + z_accel ** 2)) / math.pi
 
     return roll, pitch
 
 #determine movement pwm sequence from roll and pitch values
 def get_movement(roll, pitch):
-    pass
+    movement = {'left': 0, 'right': 0, 'front': 0, 'back': 0}
+
+    if (pitch <= LOW and pitch >= -LOW):
+        movement['front'] = 0
+        movement['back'] = 0
+    elif (pitch >= HIGH):
+        movement['front'] = 1
+        movement['back'] = 0
+    elif (pitch <= -HIGH):
+        movement['front'] = 0
+        movement['back'] = 1
+    elif (pitch > LOW and pitch < HIGH):
+        movement['front'] = (pitch - LOW) / (HIGH - LOW)
+        movement['back'] = 0
+    else:
+        movement['back'] = (pitch + LOW) / (-HIGH + LOW)
+        movement['front'] = 0
+
+
+    if (roll <= LOW and roll >= -LOW):
+        movement['right'] = 0
+        movement['left'] = 0
+    elif (roll >= HIGH):
+        movement['right'] = 1
+        movement['left'] = 0
+    elif (roll <= -HIGH):
+        movement['right'] = 0
+        movement['left'] = 1
+    elif (roll > LOW and roll < HIGH):
+        movement['right'] = (roll - LOW) / (HIGH - LOW)
+        movement['left'] = 0
+    else:
+        movement['left'] = (roll + LOW) / (-HIGH + LOW)
+        movement['right'] = 0
+
+    return movement
+
+def key_press_func(key, on_time):
+    kb_controller = keyboard.Controller()
+
+    kb_controller.press(key)
+    sleep(on_time)
+    kb_controller.release(key)
 
 #return new movement data
 def start_iio(dev):
+    init = time()
     data = get_data(dev)
     roll, pitch = get_roll_pitch(data)
+    #print(roll, pitch)
     movement = get_movement(roll, pitch)
-
-    print(roll, pitch)
+    #print(movement)
+    fin = time() - init
 
     #simulation loop
-    #controller = keyboard.Controller()
-    #controller.press(key=keyboard.Key.up)
-    #controller.release(key=keyboard.Key.up)
+    #thread for each keypress
+    on_time = -1
+    for move, key in [('front', 'w'), ('back', 's'), ('left', 'a'), ('right', 'd')]:
+        value = movement[move]
+        if (value > 0):
+            on_time = PWM_TIMEOUT * value
+            key_thread = Thread(target=key_press_func, args=(key, on_time, ))
+            key_thread.daemon = True
+            key_thread.start()
+            #no join, since this function shouldn't wait for the termination of a key thread
+    
+
+    #sleep is called here, because the key threads don't wait for one another AND simultaneous presses are allowed
+    if (on_time != -1):
+        if (fin < PWM_TIMEOUT):
+            sleep(PWM_TIMEOUT - fin)
+        else:
+            sleep(fin)
 
     return movement
   
